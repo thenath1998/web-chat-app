a267f4ea4a53ec55a9b9dc9a7a7e810922119c8f
thenath1998 <rkz.suman@gmail.com>
2019-01-08 14:00:17 +0530

    connection and disconnected

--- server/server.js
+++ server/server.js
@@ -1,5 +1,7 @@
 const path = require('path');
 const express = require('express');
+const socketIO = require('socket.io');
+const http = require('http');
 
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
@@ -7,9 +9,19 @@ const port = process.env.PORT || 3000;
 console.log(publicPath);
 
 let app = express();
+let server = http.createServer(app);
+let io = socketIO(server);
 
 app.use(express.static(publicPath));
 
-app.listen(port, () => {
+io.on('connection', (socket) => {
+    console.log('new user connected');
+
+    socket.on('disconnect', () => {
+        console.log('disconnected from client');
+    });
+});
+
+server.listen(port, () => {
     console.log(`server is running on port ${port}.`);
 });
